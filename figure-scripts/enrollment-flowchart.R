a9665f0a8195fbe31c98cb8055003bf858131ba6
Sophia Tan <sophiatan@berkeley.edu>
2023-11-01 22:18:40 -0700

    add

--- figure-scripts/enrollment-flowchart.R
+++ figure-scripts/enrollment-flowchart.R
@@ -4,7 +4,7 @@ library(tibble)
 data <- tibble(x = -10:100, y= -10:100)
 head(data)
 
-d <- readRDS("/Users/lgg/Box/washb/Bangladesh/Master\ Dataset/bangladesh-cleaned-master-data.RDS") %>% filter(ipv_telo==1)
+d <- readRDS("/Users/sophiatan/Library/CloudStorage/Box-Box/washb/Bangladesh/Master Dataset/bangladesh-cleaned-master-data.RDS") %>% filter(ipv_telo==1)
 exposures_y1 <- c("viol_any_t2", "life_viol_any_t3", "cesd_sum_t2")
 outcomes_y1 <- c("TS_t2") 
 exposures_y2 <- c("life_viol_any_t3","viol_12m_any_t3", "pss_sum_mom_t3", "pss_sum_dad_t3", "cesd_sum_ee_t3")
@@ -16,8 +16,9 @@ filtering <- function(row){
 
 y1_has_exposures<-d[apply(select(d, all_of(exposures_y1)), 1, filtering),]
 y1_has_both<-y1_has_exposures[apply(select(y1_has_exposures, all_of(outcomes_y1)), 1, filtering),]
-y1_clusters<-length(unique(y1_has_both$clusterid))
-y1_n<-nrow(y1_has_both)
+y1_has_both <- y1_has_both %>% group_by(tr) %>% summarise(clusterid=length(unique(clusterid)),
+                                           n=n())
+
 
 if(is.null(exposures_y2)){
   y2_has_exposures <- y1_has_exposures
@@ -25,133 +26,161 @@ if(is.null(exposures_y2)){
   y2_has_exposures <- d[apply(select(d, all_of(exposures_y2)), 1, filtering),]
 }
 y2_has_both<-y2_has_exposures[apply(select(y2_has_exposures, all_of(outcomes_y2)), 1, filtering),]
-y2_clusters<-length(unique(y2_has_both$clusterid))
-y2_n<-nrow(y2_has_both)
+y2_has_both <- y2_has_both %>% group_by(tr) %>% summarise(clusterid=length(unique(clusterid)),
+                                                          n=n())
 
+
+library(tibble)
+data <- tibble(x = 1:100, y= 1:100)
+head(data)
+
+library(dplyr)
 data %>% 
   ggplot(aes(x, y)) +
   scale_x_continuous(minor_breaks = seq(10, 100, 10)) +
-  scale_y_continuous(minor_breaks = seq(-10, 100, 10)) +
+  scale_y_continuous(minor_breaks = seq(10, 100, 10)) +
   theme_void() ->
   p
 
 p +
-  geom_rect(xmin = 25, xmax=75, ymin=96, ymax=100, color='black',
+  geom_rect(xmin = 25, xmax=75, ymin=100, ymax=104, color='black',
             fill='white', size=0.25) +
-  annotate('text', x= 50, y=98,label= '13,279 compounds assessed for eligibility', size=3) ->
+  annotate('text', x= 50, y=102,label= '13,279 compounds assessed for eligibility', size=3.5) ->#+
+  #annotate('text', x= 50, y=102,label= 'Figure S1: CONSORT Diagram for the WASH Benefits immune status and growth factor study population', size=3) ->
   p
 
 p +
-  geom_rect(xmin = 54, xmax=92, ymin=84, ymax=94, color='black',
+  geom_rect(xmin = 58, xmax=104, ymin=91, ymax=99, color='black',
             fill='white', size=0.25) +
-  annotate('text', x= 73, y=89, label= 'Excluded: 7,728 compounds \n 7,429 compounds excluded to create buffer zones\n 219 compounds did not meet enrollment criteria\n 80 compounds declined to participate', size=3) +
-  annotate('text', x= 10, y=89,label= 'Enrollment', size=4) +
-  
-  
-  geom_rect(xmin = 20, xmax=80, ymin=76, ymax=82, color='black',
+  annotate('text', x= 81, y=95,label= 'Excluded: 7,728 compounds \n 7,429 compounds excluded to create buffer zones\n 219 compounds did not meet enrollment criteria\n 80 compounds declined to participate', size=3.5) +
+  annotate('text', x= 3, y=95,label= 'Enrollment', size=4) +
+  geom_rect(xmin = 25, xmax=75, ymin=83.5, ymax=89.5, color='black',
             fill='white', size=0.25) +
-  annotate('text', x= 50, y=79,label= '720 clusters created and randomly allocated across 7 arms \n 5,551 compounds randomly allocated across 7 arms \n 2 of 7 arms selected into substudy', size=3)  +
-  annotate('text', x= 10, y=79,label= 'Allocation', size=4) +
-  
-  geom_rect(xmin = 27, xmax=73, ymin=64, ymax=74, color='black',
+  annotate('text', x= 50, y=87.5,label= '
+720 clusters created and randomly allocated across 7 arms \n 5,551 compounds randomly allocated across 7 arms \n 2 out of 7 arms selected into substudy', size=3.5)  +
+  annotate('text', x= 2.5, y=86.5,label= 'Allocation', size=4) +
+  geom_rect(xmin = 9, xmax=25, ymin=76, ymax=82, color='black',
             fill='white', size=0.25) +
-  annotate('text', x= 50, y=69,label= "paste(bold('                   Control arm, Nutrition arm, \n     Water + Sanitation + Handwashing arm and\nNutrition + Water + Sanitation + Handwashing arm'))", parse=TRUE, size=3) +
-  annotate('text', x= 50, y=68.5,label= paste('\n\n', 180+90+90+90, ' clusters \n', 1382+702+699+686, ' households', sep=""), size=3) +
-  annotate('text', x= 10, y=63,label= 'Subsample Target', size=4) +
-  
-  
-  geom_rect(xmin = 76, xmax=96, ymin=58, ymax=68, color='black',
+  annotate('text', x= 17, y=80,label= '
+Control \n 180 clusters \n 1,382 households', size=3.5) +
+  geom_rect(xmin = 71, xmax=104, ymin=76, ymax=82, color='black',
             fill='white', size=0.25) +
-  annotate('text', x= 86, y=64.8,label= "paste(bold('Number of clusters not \n selected into substudy'))", parse=TRUE, size=3) + 
-  annotate('text', x= 86, y=63.4,label= "paste(bold('Year 1 '))", parse=TRUE, size=3) + 
-  annotate('text', x= 86, y=62.1,label= paste(180-68+90-64+90-63+90-63, ' clusters', sep=""), size=3) + 
-  annotate('text', x= 86, y=60.9,label= "paste(bold('Year 2'))", parse=TRUE, size=3) + 
-  annotate('text', x= 86, y=59.7,label= paste(180-68+90-66+90-66+90-67, ' clusters', sep=""), size=3) +
-  
-  geom_rect(xmin = 42, xmax=58, ymin=52, ymax=62, color='black',
+  annotate('text', x= 88, y=80,label= '
+Nutrition+Water+Sanitation+Handwashing \n 90 clusters \n 686 households ', size=3.5) +
+  geom_rect(xmin = 71, xmax=104, ymin=63, ymax=75, color='black',
             fill='white', size=0.25) +
-  annotate('text', x=50, y=61.2,label= "paste(bold('Year 1'))", parse=TRUE, size=3) + 
-  annotate('text', x=50, y=60.2,label= paste('\n\n', 68+64+63+63, ' clusters \n', 516+496+486+480, ' children', sep=""), size=3) +
-  annotate('text', x=50, y=56.2,label= "paste(bold('Year 2'))", parse=TRUE, size=3) + 
-  annotate('text', x=50, y=55.2,label= paste('\n\n', 68+66+66+67, ' clusters \n', 516+514+514+505, ' children', sep=""), size=3) +
-  
-  
-  geom_rect(xmin = 37, xmax=63, ymin=26, ymax=50, color='black',
+  annotate('text', x= 88, y=70,label= '
+Year 1 \n 63 clusters \n 480 children \n Year 2 \n 67 clusters \n 505 children ', size=3.5)+
+  geom_rect(xmin = 71, xmax=104, ymin=32, ymax=62, color='black',
             fill='white', size=0.25) +
-  annotate('text', x= 50, y=48.9,label= "paste(bold('Year 1'))", parse=TRUE, size=3) + 
-  annotate('text', x= 50, y=45.1,label= paste('\n\n',140+101+107+100,' children lost to follow-up \n',14+13+17+9,' moved \n',16+29+17+14,' absent \n',62+17+23+14,' withdrew \n',29+30+32+37,' no live birth \n',19+24+21+11,' child death ', sep=""), size=3) + 
-  annotate('text', x= 50, y=37.7,label= "paste(bold('Year 2'))", parse=TRUE, size=3) + 
-  annotate('text', x= 50, y=32.3,label= paste('\n',25+18+28,' new children measured \n',158+104+115+142,' children lost to follow-up \n',35+22+36+28,' moved \n',3+4+9+2,' absent \n',72+30+42+18,' withdrew \n',29+32+35+38,' no live birth \n',19+27+20+18,' child death ', sep=""), size=3) + 
-  annotate('text', x= 10, y=38.1,label= 'Follow-up', size=4) +
-  
-  geom_rect(xmin = 42, xmax=58, ymin=14, ymax=24, color='black',
+  annotate('text', x= 88, y=48,label= '
+Year 1 \n 100 children lost to follow-up \n 9 moved \n 29 absent \n 14 withdrew \n 37 no live birth \n 11 child death \n Year 2 \n 25 new children measured  \n 104 children lost to follow-up \n 28 moved \n 2 absent \n 18 withdrew \n 38 no live birth \n 18 child death ', size=3.5) +
+  geom_rect(xmin = 71, xmax=104, ymin=19, ymax=31, color='black',
             fill='white', size=0.25) +
-  annotate('text', x= 50, y=23.2,label= "paste(bold('Year 1'))", parse=TRUE, size=3) +
-  annotate('text', x= 50, y=22.2,label= paste('\n\n',68+64+63+63,' clusters \n',376+395+379+380,' children', sep=""), size=3) + 
-  annotate('text', x= 50, y=18.2,label= "paste(bold('Year 2'))", parse=TRUE, size=3) +
-  annotate('text', x= 50, y=17.2,label= paste('\n\n',68+66+66+67,' clusters \n',358+399+372+401,' children', sep=""), size=3) + 
-  annotate('text', x= 10, y=19,label= 'Subsample Enrollment', size=4) +
-  
-  
-  geom_rect(xmin = 36, xmax=64, ymin=4, ymax=12, color='black',
+  annotate('text', x= 88, y=26,label= '
+Year 1 \n 63 clusters \n 380 children \n Year 2 \n 67 clusters \n 401 children ', size=3.5) + 
+  geom_rect(xmin = 71, xmax=104, ymin=10, ymax=18, color='black',
             fill='white', size=0.25) +
-  annotate('text', x= 50, y=10.2,label= "paste(bold('Year 1'))", parse=TRUE, size=3) + 
-  annotate('text', x= 50, y=9.6,label= paste("\n", 376+395+379+380-y1_n, ' missing exposure or outcome*', sep=""), size=3) + 
-  annotate('text', x= 50, y=7.2,label= "paste(bold('Year 2'))", parse=TRUE, size=3) + 
-  annotate('text', x= 50, y=6.6,label= paste("\n", 358+399+372+401-y2_n, ' missing exposure or outcome', sep=""), size=3) + 
-  annotate('text', x= 10, y=8,label= 'Specimen Collection', size=4) +
-  
-  
-  geom_rect(xmin = 42, xmax=58, ymin=-8, ymax=2, color='black',
+  annotate('text', x= 88, y=15,label= '
+Year 1 \n 39 missing outcome \n Year 2 \n 23 missing outcome', size=3.5) + 
+  geom_rect(xmin = 71, xmax=104, ymin=-3, ymax=9, color='black',
             fill='white', size=0.25) +
-  annotate('text', x= 50, y=1.2,label= "paste(bold('Year 1'), sep='')", parse=T, size=3) +
-  annotate('text', x= 50, y=.2,label= paste("\n\n", y1_clusters, ' clusters \n ', y1_n, ' children', sep=''), size=3) +
-  annotate('text', x= 50, y=-3.8,label= "paste(bold('Year 2'), sep='')", parse=T, size=3) +
-  annotate('text', x= 50, y=-4.8,label= paste("\n\n", y2_clusters, ' clusters \n ', y2_n, ' children', sep=''), size=3) +
-  annotate('text', x= 10, y=-3,label= 'Analysis', size=4) ->
+  annotate('text', x= 88, y=4,label= '
+Year 1 \n 62 clusters \n 341 children \n Year 2 \n 67 clusters \n 378 children', size=3.5) +
+  geom_rect(xmin = 9, xmax=25, ymin=63, ymax=75, color='black',
+            fill='white', size=0.25) +
+  annotate('text', x= 17, y=70,label= '
+Year 1 \n 68 clusters \n 516 children \n Year 2 \n 68 clusters \n 516 children ', size=3.5) +
+  annotate('text', x= 3, y=70,label= 'Subsample \n Target', size=3.5) +
+  geom_rect(xmin = 6, xmax=28, ymin=32, ymax=62, color='black',
+            fill='white', size=0.25) +
+  annotate('text', x= 17, y=48,label= '
+Year 1 \n 140 children lost to follow-up \n 14 moved \n 16 absent \n 62 withdrew \n 29 no live birth \n 19 child death \n Year 2 \n 0 new children measured  \n 158 children lost to follow-up \n 35 moved \n 3 absent \n 72 withdrew \n 29 no live birth \n 19 child death ', size=3.5) +
+  annotate('text', x= 0, y=49,label= 'Follow-up', size=3.3) +
+  geom_rect(xmin = 9, xmax=25, ymin=19, ymax=31, color='black',
+            fill='white', size=0.25) +
+  annotate('text', x= 17, y=26,label= '
+Year 1 \n 68 clusters \n 376 children \n Year 2 \n 68 clusters \n 358 children ', size=3.5) +
+  annotate('text', x= 2.5, y=26,label= 'Subsample \n Enrollment', size=3.5) +
+  geom_rect(xmin = 9, xmax=25, ymin=10, ymax=18, color='black',
+            fill='white', size=0.25) +
+  annotate('text', x= 17, y=15,label= '
+Year 1 \n 57 missing outcome \n Year 2 \n 34 missing outcome', size=3.5) +
+  annotate('text', x= 2.5, y=15,label= 'Specimen \n Collection', size=3.5) +
+  annotate('text', x= 2.5, y=4,label= 'Analysis', size=3.5) +
+  geom_rect(xmin = 9, xmax=25, ymin=-3, ymax=9, color='black',
+            fill='white', size=0.25) +
+  annotate('text', x= 17, y=4,label= '
+Year 1 \n 68 clusters \n 319 children \n Year 2 \n 68 clusters \n 324 children', size=3.5) ->
   p
+p
 
 p +
   geom_segment(
-    x=50, xend=50, y=96, yend=82, 
+    x=50, xend=50, y=100, yend=89.5, 
     size=0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type= "closed")) +
   geom_segment(
-    x=50, xend=54, y=89, yend=89, 
+    x=50, xend=58, y=95, yend=95, 
     size=0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type= "closed")) +
   geom_segment(
-    x=50, xend=50, y=76, yend=74, 
+    x=17, xend=17, y=76, yend=75, 
     size=0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type= "closed")) +
   geom_segment(
-    x=50, xend=50, y=64, yend=62, 
+    x=17, xend=17, y=63, yend=62, 
     size=0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type= "closed")) +
   geom_segment(
-    x=50, xend=76, y=63, yend=63, 
+    x=17, xend=17, y=32, yend=31, 
     size=0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type= "closed")) +
   geom_segment(
-    x=50, xend=50, y=52, yend=50, 
+    x=17, xend=17, y=19, yend=18, 
     size=0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type= "closed")) +
   geom_segment(
-    x=50, xend=50, y=26, yend=24, 
+    x=17, xend=17, y=10, yend=9, 
     size=0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type= "closed")) +
   geom_segment(
-    x=50, xend=50, y=14, yend=12, 
+    x=88, xend=88, y=76, yend=75, 
     size=0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type= "closed")) +
   geom_segment(
-    x=50, xend=50, y=4, yend=2, 
+    x=88, xend=88, y=63, yend=62, 
+    size=0.15, linejoin = "mitre", lineend = "butt",
+    arrow = arrow(length = unit(1, "mm"), type= "closed")) +
+  geom_segment(
+    x=88, xend=88, y=32, yend=31, 
+    size=0.15, linejoin = "mitre", lineend = "butt",
+    arrow = arrow(length = unit(1, "mm"), type= "closed")) +
+  geom_segment(
+    x=88, xend=88, y=19, yend=18, 
+    size=0.15, linejoin = "mitre", lineend = "butt",
+    arrow = arrow(length = unit(1, "mm"), type= "closed")) +
+  geom_segment(
+    x=88, xend=88, y=10, yend=9, 
+    size=0.15, linejoin = "mitre", lineend = "butt",
+    arrow = arrow(length = unit(1, "mm"), type= "closed")) +
+  geom_segment(
+    x=25, xend=17, y=86.5, yend=86.5, 
+    size=0.15, linejoin = "mitre", lineend = "butt") + 
+  geom_segment(
+    x=17, xend=17, y=86.5, yend=82, 
+    size=0.15, linejoin = "mitre", lineend = "butt",
+    arrow = arrow(length = unit(1, "mm"), type= "closed")) +
+  geom_segment(
+    x=75, xend=88, y=86.5, yend=86.5, 
+    size=0.15, linejoin = "mitre", lineend = "butt") + 
+  geom_segment(
+    x=88, xend=88, y=86.5, yend=82, 
     size=0.15, linejoin = "mitre", lineend = "butt",
     arrow = arrow(length = unit(1, "mm"), type= "closed")) ->
   p
+p
 
-p <- p +
-  annotate("text", x= 50, y=-10,label= "*This substudy did not assess stress measures in the Nutrition and the Water, Sanitation, and Handwashing arms at Year 1", size=3)
 
 # YOU MAY NEED TO CHANGE THE FILE PATHS HERE
 ggsave(p, file = here("figures/enrollment_figure1.jpg"), height=14, width=9)
