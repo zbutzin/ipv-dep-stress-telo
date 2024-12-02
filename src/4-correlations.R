36b542b03acf3150e5a5e0c83533002e4c4f0d1b
Andrew Mertens <amertens@berkeley.edu>
2024-12-02 15:36:17 -0800

    reviewer response

--- src/4-correlations.R
+++ src/4-correlations.R
@@ -2,59 +2,7 @@ rm(list=ls())
 
 source(here::here("0-config.R"))
 
-d <- readRDS(paste0(dropboxDir,"Data/Cleaned/Audrie/ipv-dep-stress-telo-covariates.RDS"))
-
-#correlations for reviewer response:
-# What was the correlation between maternal IPV, depression and parental PSS?
-# Are their correlations consistent with the effect direction with telomere length?
-corr.test(d$viol_any_preg, d$cesd_sum_t2)
-corr.test(d$viol_any_preg, d$cesd_sum_ee_t3)
-corr.test(d$viol_any_t2, d$cesd_sum_ee_t3)
-corr.test(d$viol_any_t2, d$cesd_sum_t2)
-
-print(corr.test(d$life_viol_any_t3, d$cesd_sum_ee_t3),short=FALSE)
-print(corr.test(d$life_viol_any_t3, d$pss_sum_mom_t3),short=FALSE)
-print(corr.test(d$life_viol_any_t3, d$pss_sum_dad_t3),short=FALSE)
-print(corr.test(d$cesd_sum_ee_t3, d$pss_sum_mom_t3),short=FALSE)
-print(corr.test(d$cesd_sum_ee_t3, d$pss_sum_dad_t3),short=FALSE)
-print(corr.test(d$cesd_sum_ee_t3, d$delta_TS_Z),short=FALSE)
-print(corr.test(d$pss_sum_mom_t3, d$delta_TS_Z),short=FALSE)
-print(corr.test(d$pss_sum_dad_t3, d$delta_TS_Z),short=FALSE)
-print(corr.test(d$life_viol_any_t3, d$TS_t3_Z),short=FALSE)
-
-round(cor(d$life_viol_any_t3, d$cesd_sum_ee_t3, use = "pairwise.complete.obs"),3)
-round(cor(d$life_viol_any_t3, d$pss_sum_mom_t3, use = "pairwise.complete.obs"),3)
-round(cor(d$life_viol_any_t3, d$pss_sum_dad_t3, use = "pairwise.complete.obs"),3)
-round(cor(d$cesd_sum_ee_t3, d$pss_sum_mom_t3, use = "pairwise.complete.obs"),3)
-round(cor(d$cesd_sum_ee_t3, d$pss_sum_dad_t3, use = "pairwise.complete.obs"),3)
-
-print_cor <- function(x,y, xlab, ylab){
-  return(data.frame(X=xlab, Y=ylab, r=round(cor(d[[x]], d[[y]], use = "pairwise.complete.obs"),3)))
-}
-
-cor_tab <- bind_rows(
-  print_cor("life_viol_any_t3", "cesd_sum_ee_t3", "Lifetime maternal IPV", "Maternal depression (CESD)"),
-  print_cor("life_viol_any_t3", "pss_sum_mom_t3", "Lifetime maternal IPV", "Maternal stress  (PSS)"),
-  print_cor("life_viol_any_t3", "pss_sum_dad_t3", "Lifetime maternal IPV", "Paternal stress (PSS)"),
-  print_cor("cesd_sum_ee_t3", "pss_sum_mom_t3", "Maternal depression (CESD)", "Maternal stress  (PSS)"),
-  print_cor("cesd_sum_ee_t3", "pss_sum_dad_t3", "Maternal depression (CESD)", "Paternal stress (PSS)"),
-  print_cor("life_viol_any_t3", "delta_TS_Z", "Lifetime maternal IPV", "Change in Telomere Length"),
-  print_cor("cesd_sum_ee_t3", "delta_TS_Z", "Maternal depression (CESD)", "Change in Telomere Length"),
-  print_cor("pss_sum_mom_t3", "delta_TS_Z", "Maternal stress  (PSS)", "Change in Telomere Length"),
-  print_cor("pss_sum_dad_t3", "delta_TS_Z", "Paternal stress (PSS)", "Change in Telomere Length"),
-  print_cor("life_viol_any_t3", "TS_t2_Z", "Lifetime maternal IPV", "Telomere Length - T2"),
-  print_cor("cesd_sum_ee_t3", "TS_t2_Z", "Maternal depression (CESD)", "Telomere Length - T2"),
-  print_cor("pss_sum_mom_t3", "TS_t2_Z", "Maternal stress  (PSS)", "Telomere Length - T2"),
-  print_cor("pss_sum_dad_t3", "TS_t2_Z", "Paternal stress (PSS)", "Telomere Length - T2"),
-  print_cor("life_viol_any_t3", "TS_t3_Z", "Lifetime maternal IPV", "Telomere Length - T3"),
-  print_cor("cesd_sum_ee_t3", "TS_t3_Z", "Maternal depression (CESD)", "Telomere Length - T3"),
-  print_cor("pss_sum_mom_t3", "TS_t3_Z", "Maternal stress  (PSS)", "Telomere Length - T3"),
-  print_cor("pss_sum_dad_t3", "TS_t3_Z", "Paternal stress (PSS)", "Telomere Length - T3")
-)
-
-knitr::kable(cor_tab)
-
-
+#d <- readRDS(paste0(dropboxDir,"Data/Cleaned/Audrie/ipv-dep-stress-telo-covariates.RDS"))
 
 #Set list of adjustment variables
 #Make vectors of adjustment variable names
